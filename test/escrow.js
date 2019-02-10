f865aba81cd759d13e25233d4a89773fded4395e
Jun Katagiri <katagiri.jun2@gmail.com>
2019-02-10 13:41:24 +0900

    modified configuration and passed test

--- test/escrow.js
+++ test/escrow.js
@@ -1,13 +1,15 @@
 const Escrow = artifacts.require("Escrow");
 
-contract("Escrow", ([owner, nonOwner]) => {
-  it("...should deposit the value 100.", async () => {
+contract("Escrow", ([account, payee]) => {
+  it("...should deposit the value 1000.", async () => {
+    const amount = 1000;
+
     const escrow = await Escrow.deployed();
 
-    await escrow.deposit(owner), { from: owner, value: 100 };
+    await escrow.deposit(payee, {from: account, value: amount});
 
-    const depositsOf = await escrow.depositsOf(owner);
+    const depositsOf = await escrow.depositsOf(payee);
 
-    assert.equal(depositsOf, 100, "The value 100 was not deposited.");
+    assert.equal(depositsOf, amount, "The value " + amount + " was not deposited.");
   });
 });
